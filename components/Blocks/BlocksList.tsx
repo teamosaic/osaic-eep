60016a2b708402cc3d3e9819fc52ad643997cdc2
Robert Reinhard <robert.reinhard@bukwild.com>
2023-03-31 12:49:42 -0700

    Make function names more explicit

--- components/Blocks/BlocksList.tsx
+++ components/Blocks/BlocksList.tsx
@@ -12,18 +12,18 @@ interface BlocksListProps {
 export default function BlocksList({
 	blocks = []
 }: BlocksListProps): React.ReactElement {
-	return <>{ blocks.map(makeBlock) }</>
+	return <>{ blocks.map(makeBlockInstance) }</>
 }
 
 // Return a block depending on the _type
-function makeBlock(block: any): React.ReactElement {
-	const ComponentFunction = getComponent(block._type)
+function makeBlockInstance(block: any): React.ReactElement {
+	const ComponentFunction = getComponentFunction(block._type)
 	if (!ComponentFunction) return <div>Unknown block { block._type }</div>
 	return <ComponentFunction block={ block } key={ block._key} />
 }
 
 // Return the component class based on the type
-function getComponent(type: string): Function {
+function getComponentFunction(type: string): Function {
 	switch(type) {
 		case 'marqueeBlock': return MarqueeBlockComponent
 		case 'spacerBlock': return SpacerBlockComponent
