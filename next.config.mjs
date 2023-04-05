8bf35102b389120d2a9bab83b7ec6dc2c8514528
Robert Reinhard <robert.reinhard@bukwild.com>
2023-04-05 16:41:33 -0700

    Enable reactStrictMode

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const config = {
+
+  // Get warnings on deprecations, etc
+  // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
+  reactStrictMode: true,
+
+  // Serve images from Sanity's image CDN
   images: {
-		domains: ['cdn.sanity.io']
-	}
+    domains: ['cdn.sanity.io']
+  }
 }
 
 export default config
