71e75b920d6e7f50e467ec8b13d218ee945a17b4
Robert Reinhard <robert.reinhard@bukwild.com>
2023-04-06 17:07:41 -0700

    Commenting

--- sanity/components/PagePeview.tsx
+++ sanity/components/PagePeview.tsx
@@ -1,6 +1,7 @@
 import { PreviewSuspense } from 'next-sanity/preview'
 import dynamic from 'next/dynamic'
 
+// Load on demand so it's deps are not bundled when not previewing
 const PreviewLoader = dynamic(() => import('~/sanity/components/PreviewLoader'))
 
 export default function PagePreview({ previewToken, uri, render }) {
