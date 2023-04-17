7c82e01bddbe6a1492a32bad2316dbdb52cee6b5
Robert Reinhard <robert.reinhard@bukwild.com>
2023-04-17 08:02:59 -0700

    Move bigger margins into “article” prose theme

---
+++ styles/prose.js
@@ -0,0 +1,99 @@
+import { fluid, inheritProseColor } from '../packages/style-utils'
+import merge from 'lodash/merge'
+
+// Base prose / typography styles
+export const base = {
+
+  // Stop prose from specifying it's own color values
+  ...inheritProseColor,
+
+  // Make all margins the same, for use in marquees and other places.
+  '*': {
+    marginTop: '1rem',
+    marginBottom: '1rem',
+  },
+
+  // Re-apply the margin clearing on first/last
+  '> :first-child': { marginTop: 0 },
+  '> :last-child': { marginBottom: 0 },
+
+  // Make fluid font sizes based on Cloak styleguide
+  h1: {
+    fontSize: fluid(100, 70),
+    letterSpacing: '-0.07em',
+    lineHeight: fluid(100, 70),
+    fontWeight: 400,
+  },
+  h2: {
+    fontSize: fluid(70, 50),
+    letterSpacing: '-0.07em',
+    lineHeight: fluid(70, 50),
+    fontWeight: 400,
+  },
+  h3: {
+    fontSize: fluid(50, 36),
+    letterSpacing: '-0.06em',
+    lineHeight: fluid(55, 40),
+    fontWeight: 400,
+  },
+  h4: {
+    fontSize: fluid(36, 28),
+    letterSpacing: '-0.03em',
+    lineHeight: fluid(40, 32),
+    fontWeight: 400,
+  },
+  h5: {
+    fontSize: fluid(28, 20),
+    letterSpacing: '-0.02em',
+    lineHeight: fluid(28, 20),
+    fontWeight: 400,
+  },
+  h6: {
+    fontSize: fluid(28, 14),
+    lineHeight: fluid(28, 22),
+    fontWeight: 600,
+  },
+  p: {
+    fontSize: fluid(16, 14),
+    lineHeight: fluid(24, 22),
+  },
+}
+
+// Adds font-size relative margins to the elements for article-style cases
+export const article = {
+
+  // Make fluid font sizes based on Cloak styleguide
+  h1: {
+    marginTop: '0.7em',
+    marginBottom: '0.7em',
+  },
+  h2: {
+    marginTop: '0.8em',
+    marginBottom: '0.8em',
+  },
+  h3: {
+    marginTop: '0.9em',
+    marginBottom: '0.9em',
+  },
+  h4: {
+    marginTop: '1em',
+    marginBottom: '1em',
+  },
+  h5: {
+    marginTop: '1.1em',
+    marginBottom: '1.1em',
+  },
+  h6: {
+    marginTop: '1.3em',
+    marginBottom: '1.3em',
+  },
+  p: {
+    marginTop: '1.5em',
+    marginBottom: '1.5em',
+  },
+
+  // Re-apply the margin clearing on first/last
+  '> :first-child': { marginTop: 0 },
+  '> :last-child': { marginBottom: 0 },
+
+}
--- types/optionTypes.ts
+++ types/optionTypes.ts
@@ -30,3 +30,8 @@ export enum BackgroundColor {
   None = 'none',
   Dark = 'dark',
 }
+
+export enum CopyThemes {
+  Basic = 'basic',
+  Article = 'article',
+}
