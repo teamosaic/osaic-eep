89c2f24f68308d8c925d070ad171c56e2d3053e0
Robert Reinhard <robert.reinhard@bukwild.com>
2023-04-20 17:35:23 -0700

    Wiring up background image

--- components/blocks/heroes/SimpleCenteredWithBackgroundImageHeroBlock.tsx
+++ components/blocks/heroes/SimpleCenteredWithBackgroundImageHeroBlock.tsx
@@ -1,13 +1,24 @@
+import SanityImage from '~/packages/sanity-image/SanityImage'
+import AnimateInView from '~/packages/animate-in-view'
+
 // Based on
 // https://tailwindui.com/components/marketing/sections/heroes#component-d63f5b5552a3f3d936c6ab970a47899b
-export default function SimpleCenteredWithBackgroundImageHeroBlock() {
+export default function SimpleCenteredWithBackgroundImageHeroBlock({ block }: {
+  block: any
+}): React.ReactElement {
   return (
     <div className="relative isolate overflow-hidden pt-14">
-      <img
-        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-        alt=""
-        className="absolute inset-0 -z-10 h-full w-full object-cover"
-      />
+
+      {/* Background image */}
+      { block.background &&
+        <AnimateInView className='animate-slow-scale-down-in absolute inset-0'>
+          <SanityImage
+            expand
+            priority
+            sizes='100vw'
+            source={ block.background } />
+        </AnimateInView> }
+
       <div
         className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
         aria-hidden="true"
@@ -20,7 +31,8 @@ export default function SimpleCenteredWithBackgroundImageHeroBlock() {
           }}
         />
       </div>
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 relative">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
           <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
             Announcing our next round of funding.{' '}
@@ -51,6 +63,7 @@ export default function SimpleCenteredWithBackgroundImageHeroBlock() {
           </div>
         </div>
       </div>
+
       <div
         className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
         aria-hidden="true"
