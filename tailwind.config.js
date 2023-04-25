657fce6d5b0c229335443f1be127b8131a27f296
Robert Reinhard <robert.reinhard@bukwild.com>
2023-04-24 21:02:56 -0700

    Adding layouts to tailwind sources

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
     './sanity/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
     './packages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
