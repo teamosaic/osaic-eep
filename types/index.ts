91821f846004a4a1b9a50edc7298f7598dabde45
Michael Starks <michael.starks@bkwld.com>
2023-06-13 15:25:34 -0600

    Splitting the blockTypes file to individual ones

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,14 @@
-export * from './blockTypes'
-export * from './objectTypes'
+export * from './articlesBlockTypes'
 export * from './componentTypes'
+export * from './ctaBlockTypes'
 export * from './fieldGroupTypes'
+export * from './heroBlockTypes'
+export * from './objectTypes'
 export * from './pageTypes'
 export * from './sanityTypes'
+
+import * as ArticlesBlock from './articlesBlockTypes'
+import * as CtaBlock from './ctaBlockTypes'
+import * as HeroBlock from './heroBlockTypes'
+
+export type Block = HeroBlock | CtaBlock | ArticlesBlock
