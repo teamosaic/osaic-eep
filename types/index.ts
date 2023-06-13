23ad65b595f2b202c1a4642c6b80693311ecb687
Michael Starks <michael.starks@bkwld.com>
2023-06-13 15:56:57 -0600

    Moving blockTypes files to subdirectory

--- types/index.ts
+++ types/index.ts
@@ -1,14 +1,6 @@
-export * from './articlesBlockTypes'
+export * from './blockTypes'
 export * from './componentTypes'
-export * from './ctaBlockTypes'
 export * from './fieldGroupTypes'
-export * from './heroBlockTypes'
 export * from './objectTypes'
 export * from './pageTypes'
 export * from './sanityTypes'
-
-import * as ArticlesBlock from './articlesBlockTypes'
-import * as CtaBlock from './ctaBlockTypes'
-import * as HeroBlock from './heroBlockTypes'
-
-export type Block = HeroBlock | CtaBlock | ArticlesBlock
