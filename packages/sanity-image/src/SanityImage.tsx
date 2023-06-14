e8a43818497f993a76b8f72bd23959a16f8614af
Robert Reinhard <robert.reinhard@bukwild.com>
2023-06-13 17:50:20 -0700

    Fix case of SanityImage used outside of a block

--- packages/sanity-image/src/SanityImage.tsx
+++ packages/sanity-image/src/SanityImage.tsx
@@ -22,8 +22,9 @@ export default function SanityImage({
   source, expand, aspect, width, height, priority, sizes, fit, className = '',
 }: SanityImageProps): React.ReactElement | null {
 
-  // Force image priority if the block's index is <= 1 (the first and 2nd blocks)
-  if (priority == undefined && useContext(BlockOrderContext).index <= 1) {
+  // Force image priority if the block's index is <= 1 (the first and 2nd
+  // blocks)
+  if (priority == undefined && useContext(BlockOrderContext)?.index <= 1) {
     priority = true;
   }
 
