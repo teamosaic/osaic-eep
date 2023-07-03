3c3b4c75ec054718925769223020d89e449bc5a6
Robert Reinhard <robert.reinhard@bukwild.com>
2023-07-03 15:52:40 -0700

    Switch to Sanity v5 way of fetching preview data

--- sanity/components/PagePeview.tsx
+++ sanity/components/PagePeview.tsx
@@ -1,36 +1,17 @@
 import dynamic from 'next/dynamic'
-import { PreviewSuspense } from 'next-sanity/preview'
+
+import PreviewProvider from './PreviewProvider'
 
 // Load on demand so it's deps are not bundled when not previewing
 const PreviewLoader = dynamic(() => import('~/sanity/components/PreviewLoader'))
 
-export default function PagePreview({ previewToken, query, params, render }) {
+export default function PagePreview({
+  initialData, previewToken, query, params, render
+}) {
   return (
-    <PreviewSuspense fallback={ <Placeholder /> }>
-      <PreviewLoader {...{ previewToken, query, params, render }} />
-    </PreviewSuspense>
+    <PreviewProvider token={ previewToken }>
+      <PreviewLoader {...{ initialData, query, params, render }} />
+    </PreviewProvider>
   )
 }
 
-function Placeholder() {
-  return (
-    <div className='grid h-screen place-items-center p-8'>
-      <div className='flex'>
-        <span className='mr-2'>Loading Preview</span>
-        <Spinner />
-      </div>
-    </div>
-  )
-}
-
-// https://github.com/n3r4zzurr0/svg-spinners
-function Spinner() {
-  return (
-    <svg
-      className='animate-spin'
-      width='24' height='24' viewBox='0 0 24 24'
-      xmlns='http://www.w3.org/2000/svg'>
-      <path d='M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z' fill='currentColor' />
-    </svg>
-  )
-}
--- sanity/components/PreviewLoader.tsx
+++ sanity/components/PreviewLoader.tsx
@@ -1,13 +1,21 @@
-import { usePreview } from '~/sanity/lib/preview/previewHook'
+import { useLiveQuery } from 'next-sanity/preview'
+
+import { getSettings } from '~/queries/settingsQueries'
 
 import PreviewControls from './preview-controls/PreviewControls'
 
 // Fetch preview data from Sanity and then pass it to child components
-export default function PreviewLoader({ previewToken, query, params, render }) {
+export default function PreviewLoader({ initialData, query, params, render }) {
 
   // Load draft data
-  const page = usePreview(previewToken, query, params)
-  const settings = usePreview(previewToken, `*[_type == 'settings'][0]`)
+  const [ page, pageLoading ] = useLiveQuery(initialData.page, query, params)
+  const [ settings, settingsLoading ] = useLiveQuery(initialData.settings,
+    getSettings)
+
+  // Render placeholder if loading
+  if (pageLoading || settingsLoading) {
+    return <Placeholder />
+  }
 
   // Pass draft data to children components
   return (
@@ -15,5 +23,28 @@ export default function PreviewLoader({ previewToken, query, params, render }) {
       <PreviewControls />
       { render({ page, settings }) }
     </>
+  )
+}
+
+function Placeholder() {
+  return (
+    <div className='grid h-screen place-items-center p-8'>
+      <div className='flex'>
+        <span className='mr-2'>Loading Preview</span>
+        <Spinner />
+      </div>
+    </div>
+  )
+}
+
+// https://github.com/n3r4zzurr0/svg-spinners
+function Spinner() {
+  return (
+    <svg
+      className='animate-spin'
+      width='24' height='24' viewBox='0 0 24 24'
+      xmlns='http://www.w3.org/2000/svg'>
+      <path d='M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z' fill='currentColor' />
+    </svg>
   )
 }
