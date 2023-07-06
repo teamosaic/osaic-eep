cb8606079d2e8d34eaa64f49f88dd730d41fb333
Matthew Aebersold <matt.aebersold@bkwld.com>
2023-07-06 12:10:41 -0700

    Setting up boilerplate gtm

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const config = {
 
+  env: {
+    GTM_ID: process.env.GTM_ID,
+  },
+
   // Get warnings on deprecations, etc
   // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
   reactStrictMode: true,
