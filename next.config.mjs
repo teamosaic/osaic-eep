df38c3a5199ef4c900ed89a23d86f67d9f018fb5
Matthew Aebersold <matt.aebersold@bkwld.com>
2023-07-07 11:15:02 -0700

    Updates to GTM integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const config = {
 
-  env: {
-    GTM_ID: process.env.GTM_ID,
-  },
-
   // Get warnings on deprecations, etc
   // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
   reactStrictMode: true,
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import 'styles/global.css'
 
+import GoogleTagManager from 'components/layout/GoogleTagManager'
 import { AppProps } from 'next/app'
 // Example of using Google fonts via @next/font with a variable width font
 import { Inter } from 'next/font/google'
-import Script from 'next/script'
 
 const sans = Inter({
   variable: '--font-sans',
@@ -23,15 +23,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </style>
 
       {/* Google Tag Manager Script */}
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${process.env.GTM_ID}');
-        `}
-      </Script>
+      <GoogleTagManager />
 
       <Component {...pageProps} />
     </>
