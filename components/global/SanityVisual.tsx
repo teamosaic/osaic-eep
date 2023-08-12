fbbaff61ed1183ab9bed8a53c58107bccd1d4551
Robert Reinhard <robert.reinhard@bukwild.com>
2023-08-11 19:42:53 -0700

    Restore using BlockOrderContext to auto-set priority

--- components/global/SanityVisual.tsx
+++ components/global/SanityVisual.tsx
@@ -1,14 +1,22 @@
 import SanityNextVisual, {
   type SanityNextVisualProps
 } from '@react-visual/sanity-next'
+import { useContext } from 'react'
+
+import { BlockOrderContext } from '~/providers/blockOrder'
 
 // Apply app-specific customizations to @react-visual/sanity-next
 export default function SanityVisual(
   props: SanityNextVisualProps
 ): React.ReactElement | null {
 
+  // Auto-set priority if the block's index is <= 1 (the first and 2nd blocks)
+  const blockOrder = useContext(BlockOrderContext)
+  const priority = props.priority || blockOrder.index <= 1
+
+  // Render @react-visual/sanity-next
   return <SanityNextVisual {...{
     ...props,
+    priority,
   }} />
-
 }
