6c2baeaedfda22ad5a6e0a286790ff4b65cbb952
Robert Reinhard <robert.reinhard@bukwild.com>
2023-08-21 17:09:56 -0700

    Break up FAQ into multiple components

--- components/blocks/FaqBlock.tsx
+++ components/blocks/FaqBlock.tsx
@@ -1,15 +1,16 @@
 import { Disclosure } from '@headlessui/react'
 import { MinusSmallIcon, PlusSmallIcon } from '@heroicons/react/24/outline'
+import { ReactNode } from 'react'
 
 import MarketingPortableText from '~/components/global/portableText/MarketingPortableText'
 import AnimateInView from '~/packages/animate-in-view'
-import { FaqBlock as BlockType } from '~/types'
+import { Faq,FaqBlock as BlockType } from '~/types'
 
 
 export default function FaqBlock({
   title,
   faqs,
-}: BlockType): React.ReactElement {
+}: BlockType): ReactNode {
   return (
     <div className="bg-gray-900">
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
@@ -17,35 +18,49 @@ export default function FaqBlock({
           <h2 className="text-2xl font-bold leading-10 tracking-tight text-white">{title}</h2>
           <dl className="mt-10 space-y-6 divide-y divide-white/10">
             {faqs.map((faq) => (
-              <Disclosure as="div" key={faq.question} className="pt-6">
-                {({ open }) => (
-                  <>
-                    <dt>
-                      <Disclosure.Button className="flex w-full items-start justify-between text-left text-white">
-                        <span className="text-base font-semibold leading-7">{faq.question}</span>
-                        <span className="ml-6 flex h-7 items-center">
-                          {open ? (
-                            <MinusSmallIcon className="h-6 w-6" aria-hidden="true" />
-                          ) : (
-                            <PlusSmallIcon className="h-6 w-6" aria-hidden="true" />
-                          )}
-                        </span>
-                      </Disclosure.Button>
-                    </dt>
-                    <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                      <AnimateInView
-                        target='descendants'
-                        className='prose-animate-in text-white'>
-                        <MarketingPortableText value={ faq.answer }/>
-                      </AnimateInView>
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
+              <FaqAccordion {...faq} key={ faq._key } />
             ))}
           </dl>
         </div>
       </div>
     </div>
   )
+}
+
+function FaqAccordion({ question, answer }: Faq): ReactNode {
+  return (
+    <Disclosure as="div" className="pt-6">
+      {({ open }) => (
+        <>
+
+          {/* Question and toggle bar */}
+          <dt>
+            <Disclosure.Button className="flex w-full items-start justify-between text-left text-white">
+              <span className="text-base font-semibold leading-7">
+                {question}
+              </span>
+              <span className="ml-6 flex h-7 items-center">
+                <ToggleIcon open={ open } />
+              </span>
+            </Disclosure.Button>
+          </dt>
+
+          {/* Collapsable answer */}
+          <Disclosure.Panel as="dd" className="mt-2 pr-12">
+            <AnimateInView
+              target='descendants'
+              className='prose-animate-in text-white'>
+              <MarketingPortableText value={ answer }/>
+            </AnimateInView>
+          </Disclosure.Panel>
+
+        </>
+      )}
+    </Disclosure>
+  )
+}
+
+function ToggleIcon({ open }: { open: boolean}): ReactNode {
+  if (open) return <MinusSmallIcon className="h-6 w-6" aria-hidden="true" />
+  return <PlusSmallIcon className="h-6 w-6" aria-hidden="true" />
 }
--- types/blockTypes/faqBlockTypes.ts
+++ types/blockTypes/faqBlockTypes.ts
@@ -1,16 +1,18 @@
 import type { PortableTextBlock } from '@portabletext/types'
 
+import type { SanityObject } from '~/types'
 import type {
   BlockLayout,
 } from '~/types'
 
 
-interface Faq {
-  question?: string
-  answer?: PortableTextBlock[]
-}
 
 export interface FaqBlock extends BlockLayout {
   title: string
   faqs: Faq[]
+}
+
+export interface Faq extends SanityObject {
+  question?: string
+  answer?: PortableTextBlock[]
 }
