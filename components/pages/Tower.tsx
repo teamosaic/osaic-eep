9b79aee41df06cbc57e65335354bed154a88de9a
Matthew Aebersold <matt.aebersold@bkwld.com>
2024-07-25 18:12:30 -0700

    remove hero block instances

--- components/pages/Tower.tsx
+++ components/pages/Tower.tsx
@@ -2,25 +2,13 @@ import type { Image } from 'sanity'
 
 import BlocksList from '~/components/blocks/BlocksList'
 import PageHead from '~/components/layout/PageHead'
-import { HeroBlock,Tower } from '~/types'
+import { Tower } from '~/types'
 
 export default function Tower({ page }: { page: Tower }) {
   return (
     <>
-      <PageHead { ...page } image={ getFirstHeroImage(page) } />
+      <PageHead { ...page } />
       <BlocksList blocks={ page.blocks } />
     </>
   )
 }
-
-// Get the first hero image to use as the default meta image
-function getFirstHeroImage(page: Tower): Image {
-  if (!page.blocks?.length) return
-
-  const firstHeroBlock = page.blocks
-    .find(block => block._type == 'heroBlock') as HeroBlock
-  if (!firstHeroBlock) return
-
-  return firstHeroBlock.background?.image
-}
-
--- queries/fragments/blocksFragment.ts
+++ queries/fragments/blocksFragment.ts
@@ -1,33 +1,13 @@
 import { groq } from 'next-sanity'
 
 import { visualField } from '../fields/assetFields'
-import { articleCardFragment } from './articleCardFragment'
 
 export const blocksFragment = groq`
   ...,
 
   // De-reference image fields for acccessing image metadata
-  _type == 'heroBlock' => {
-    ${ visualField('background') },
+  _type == 'enhancementsBlock' => {
+    ${ visualField('image') },
   },
 
-  // De-reference image fields for acccessing image metadata
-  _type == 'splitBlock' => {
-    ${ visualField('media') },
-  },
-
-  // Fetch recent articles
-  _type == 'articlesBlock' => {
-    'recentArticles': *[
-      _type == 'article'
-    ] | order(date desc, _id desc) [0...3]  {
-      ${ articleCardFragment },
-    },
-    'totalArticles': count(*[_type == "article"]),
-  },
-
-  // De-reference the reusable section document reference
-  _type == 'reusableSectionBlock' => {
-    reusableSection->
-  }
 `
