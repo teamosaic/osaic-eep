9e59596e76b7d119407bd9a6c4ad725f31a625e9
Matthew Aebersold <matt.aebersold@bkwld.com>
2024-09-03 09:57:01 -0700

    Fix body font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="stylesheet" href="https://use.typekit.net/exd2wbj.css"></link>
+      </Head>
       <body className='bg-ehite text-primary-text'>
         <Main />
         <NextScript />
