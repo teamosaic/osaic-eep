e07c8cb4f550e525720dafec0485f0fce16c6d38
Matthew Aebersold <matt.aebersold@bkwld.com>
2024-09-03 15:44:01 -0700

    Use correct settings interface

--- providers/settings.ts
+++ providers/settings.ts
@@ -1,13 +1,4 @@
-import { createContext } from 'react'
-import type { Image } from 'sanity'
-
-interface Settings {
-  metaTitleSuffix: string
-  metaDescription: string
-  metaImage: Image,
-  instagram: string,
-  linkedin: string,
-  twitter: string
-}
+import { createContext } from 'react';
+import { Settings } from '~/types/globalTypes';
 
 export const SettingsContext = createContext<Settings | null>(null)
