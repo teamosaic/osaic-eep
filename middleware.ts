e509242a5ac67fa20d740df782020935c6e30ad6
Matthew Aebersold <matt.aebersold@bkwld.com>
2024-09-17 14:39:00 -0700

    add logout functionality

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,15 @@ export const config = {
 
 export async function middleware(req: NextRequest) {
 
+  // if /logout is in the req url, then clear cookies, go to /welcome, and return
+  if (req.url.includes('/logout')) {
+    const response = NextResponse.redirect(new URL('/welcome', req.url))
+    response.cookies.set('next-auth.session-token', '', { maxAge: 0 })
+    response.cookies.set('__Secure-next-auth.session-token', '', { maxAge: 0 })
+    return response
+  }
+
+
   const token = await getToken({ req }) as any
   if (!token) {
     return NextResponse.redirect(new URL('/welcome', req.url))
